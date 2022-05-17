4c756574cbc3607d6f39568a166ba02c10044556
Cristian Alegre Bustos <CNAlegreBustos@teco.com.ar>
2022-05-17 13:39:55 -0300

    Simular catalogo con maps y promesas

--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -3,50 +3,10 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 import { ItemList } from './itemList.jsx';
+import { productos } from './arrayProduct.js';
 
 
 export function ItemListContainer({greeting}){
-    let  productos=[{
-        id:'1',
-        title:'Nike Air Presicion 5',
-        description:'Un primer paso rápido marca la diferencia.',
-        price:'$37900',
-        pictureURL:'./images/thumb/NikeAirPresicion5.jpg',
-        stock:5
-      },
-      {
-        id:'2',
-        title:'Nike Kyrie Fly trap 4',
-        description:'Desafia tus limites con',
-        price:'$44900',
-        pictureURL:'./images/thumb/NikeKyrieFlytrap4.jpg',
-        stock:7
-      },
-      {
-        id:'3',
-        title:'Jordan Fly Lockdown',
-        description:'Fabricado por un ganador para ganadores',
-        price:'$39900',
-        pictureURL:'./images/thumb/JordanFlyLockdown.jpg',
-        stock:4
-      },
-      {
-        id:'4',
-        title:'Under Armour Spawn 3',
-        description:'La mejor armadura para cada batalla',
-        price:'$39900',
-        pictureURL:'./images/thumb/UnderArmourSpawn3.jpg',
-        stock:4
-      },
-      {
-        id:'5',
-        title:'Adidas Pro N3XT 2021',
-        description:'Te acompaña en cada entrenamiento',
-        price:'$31900',
-        pictureURL:'./images/thumb/AdidasProN3XT2021.jpg',
-        stock:4
-      },
-    ]
     const [productList,setProductList]=useState([]);
     const[loading,setLoading]=useState(false);
     const[error,setError]=useState("");
