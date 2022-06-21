b67e322ac723c8f90e91246bdb00202806de5fc2
Cristian Alegre Bustos <CNAlegreBustos@teco.com.ar>
2022-06-21 17:36:40 -0300

    Updates

---
+++ precache-manifest.2da6220f5f41150d4d10ca0ed9dbf8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "596cbaca1992edb57aca",
+    "url": "/react_alegrecristian/static/css/main.e8118fa3.chunk.css"
+  },
+  {
+    "revision": "596cbaca1992edb57aca",
+    "url": "/react_alegrecristian/static/js/main.45414855.chunk.js"
+  },
+  {
+    "revision": "cb20187a6a68c86ef8d4",
+    "url": "/react_alegrecristian/static/js/runtime~main.00d451a1.js"
+  },
+  {
+    "revision": "e7bed1604366a64a9a2c",
+    "url": "/react_alegrecristian/static/js/2.4e8e6a65.chunk.js"
+  },
+  {
+    "revision": "b778a45604bdd0a6e7df",
+    "url": "/react_alegrecristian/static/js/3.27459842.chunk.js"
+  },
+  {
+    "revision": "1cc6aca4701f5a74168e62fc94631ea5",
+    "url": "/react_alegrecristian/index.html"
+  }
+];
